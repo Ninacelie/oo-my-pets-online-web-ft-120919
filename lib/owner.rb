14d45e2cf0c058dee50b472958e926fed4c2bb6c
Nina Angelo <ninacelie@gmail.com>
2020-01-07 21:16:29 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -58,9 +58,9 @@ class Owner
   end
 end
 
-  def list_pets 
-    number_of_dogs = self.dogs.count 
-    number_of_cats = self.cats.count 
+  def list_pets
+    number_of_dogs = self.dogs.count
+    number_of_cats = self.cats.count
   end
 
 end
