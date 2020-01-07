b3a0c2f23427b7010312f0f9ba2b62b8dd4d5b43
Nina Angelo <ninacelie@gmail.com>
2020-01-07 21:14:05 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -44,7 +44,12 @@ class Owner
   def feed_cats
     self.cats.each {|cat| cat.mood = "happy"}
   end
-  
 
+  def sell_pets
+    self.cats.each do |cat|
+      self.cats.delete(cat)
+      cat.owner = nil
+      cat.mood = "nervous"
+  end
 
 end
